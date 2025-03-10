deb8455db65d505780275b620b0f1995cf8fe855
QJin <QJin@student.saintaug.nsw.edu.au>
2025-03-11 10:54:12 +1100

    Test

--- main1.py
+++ main1.py
@@ -2,4 +2,5 @@ print("Hello")
 print("-"*30)
 print("-*-*-Welcome To Our Quiz Game-*-*-")
 print("---What's your [Name]?---")
-name = input("[Type answer here]:")
+name = input("[Type here]:")
+print("Ready for the quiz [],they're coming.") #test
